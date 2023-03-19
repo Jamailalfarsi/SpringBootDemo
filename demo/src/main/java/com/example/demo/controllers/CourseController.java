e239a36c84c84b0f24213d67dfc5395ebec0874f
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-03-19 12:32:20 +0400

    Slack Message gor course table(In active)

--- demo/src/main/java/com/example/demo/controllers/CourseController.java
+++ demo/src/main/java/com/example/demo/controllers/CourseController.java
@@ -60,10 +60,10 @@ public class CourseController {
         List<Course> InactiveCoursesList = courseService.getAllInActiveCourses();
         for(Course s:InactiveCoursesList) {
             slackClient.sendMessage("slackMessageInActive:" + s.getActive().toString());
-            slackClient.sendMessage("slackMessage_StudentId:" + s.getId().toString());
-            slackClient.sendMessage("slackMessage_StudentName:" + s.getName().toString());
-            slackClient.sendMessage("slackMassage_StudentCreatedDate" + s.getCreatedDate());
-            slackClient.sendMessage("slackMassage_StudentUpdatedDate" + s.getUpdatedDate());
+            slackClient.sendMessage("slackMessage_CourseId:" + s.getId().toString());
+            slackClient.sendMessage("slackMessage_CourseName:" + s.getName().toString());
+            slackClient.sendMessage("slackMassage_CourseCreatedDate" + s.getCreatedDate());
+            slackClient.sendMessage("slackMassage_CourseUpdatedDate" + s.getUpdatedDate());
         }
         return InactiveCoursesList;
     }
