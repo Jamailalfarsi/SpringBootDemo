51f00a0127891bc2005a54ebbab54f7166db15f2
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-03-26 12:51:05 +0400

    Added format to send message to slack in student controller for getAllStudentByIsInActive

--- demo/src/main/java/com/example/demo/Slack/SlackClient.java
+++ demo/src/main/java/com/example/demo/Slack/SlackClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class SlackClient {
     public String sendMessage(String text){
         return WebClient.create().post()
-                .uri("https://hooks.slack.com/services/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")
+                .uri("")
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(new SlackPayload(text))
                 .retrieve()
--- demo/src/main/java/com/example/demo/controllers/StudentController.java
+++ demo/src/main/java/com/example/demo/controllers/StudentController.java
@@ -55,11 +55,12 @@ public class StudentController {
     public List<Student> getAllActiveSchools() {
         List<Student> activeStudentsList = studentService.getAllActiveStudents();
         for(Student s:activeStudentsList){
-            slackClient.sendMessage("slackMessageActive:"+s.getActive().toString());
-            slackClient.sendMessage("slackMessage_StudentId:"+s.getId().toString());
-            slackClient.sendMessage("slackMessage_StudentName:"+s.getName().toString());
-            slackClient.sendMessage("slackMassage_StudentCreatedDate"+s.getCreatedDate());
-            slackClient.sendMessage("slackMassage_StudentUpdatedDate"+s.getUpdatedDate());
+//            slackClient.sendMessage("slackMessageActive:"+s.getActive().toString());
+//            slackClient.sendMessage("slackMessage_StudentId:"+s.getId().toString());
+//            slackClient.sendMessage("slackMessage_StudentName:"+s.getName().toString());
+//            slackClient.sendMessage("slackMassage_StudentCreatedDate"+s.getCreatedDate());
+//            slackClient.sendMessage("slackMassage_StudentUpdatedDate"+s.getUpdatedDate());
+            slackClient.sendMessage(studentService.formatStudentListForSlack(activeStudentsList).toString());
         }
         return activeStudentsList;
     }
