8c06ff8032deca62413a4a531fd7ccc72b2b4eb1
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-04-17 11:05:42 +0400

    Added @Repository in FeesRepository

--- demo/src/main/java/com/example/demo/Repositories/FeesRepository.java
+++ demo/src/main/java/com/example/demo/Repositories/FeesRepository.java
@@ -3,8 +3,11 @@ package com.example.demo.Repositories;
 import com.example.demo.Models.Fees;
 import com.example.demo.Models.Student;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FeesRepository extends CrudRepository<Fees,Integer> {
+
 
 
 }
