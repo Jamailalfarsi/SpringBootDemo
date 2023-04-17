43a9915c083b3e96c133672544adf58e64ef4396
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-04-17 11:04:24 +0400

    Added extedns CrudRepository in FeesRepository

--- demo/src/main/java/com/example/demo/Repositories/FeesRepository.java
+++ demo/src/main/java/com/example/demo/Repositories/FeesRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.Repositories;
 
-public interface FeesRepository {
+import com.example.demo.Models.Fees;
+import com.example.demo.Models.Student;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FeesRepository extends CrudRepository<Fees,Integer> {
+
+
 }
--- demo/src/main/java/com/example/demo/Services/FeesService.java
+++ demo/src/main/java/com/example/demo/Services/FeesService.java
@@ -1,11 +1,26 @@
 package com.example.demo.Services;
 
+import com.example.demo.Models.Fees;
+import com.example.demo.Models.School;
 import com.example.demo.Repositories.FeesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FeesService {
     @Autowired
     FeesRepository feesRepository;
+
+    public List<Fees> getAllFeeses() {
+        return feesRepository.getAllSchools();
+    }
+
+    public Fees getFeesById(Integer id) {
+        return feesRepository.getSchoolById(id);
+
+    }
+
 }
+
