cd440d5ee7dcd08d7e53c53bb32633851f8b65c4
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-04-17 10:56:35 +0400

    Added @Service and @Autowired for feesRepository in Service

--- demo/src/main/java/com/example/demo/Services/FeesService.java
+++ demo/src/main/java/com/example/demo/Services/FeesService.java
@@ -1,4 +1,11 @@
 package com.example.demo.Services;
 
+import com.example.demo.Repositories.FeesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FeesService {
+    @Autowired
+    FeesRepository feesRepository;
 }
