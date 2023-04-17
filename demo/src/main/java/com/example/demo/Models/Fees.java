d97e030b18864a16da50a3eb75c6cc75ab995f70
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-04-17 12:10:17 +0400

    Created @OneToOne to defining the foreign key which is ID

--- demo/src/main/java/com/example/demo/Models/Fees.java
+++ demo/src/main/java/com/example/demo/Models/Fees.java
@@ -4,10 +4,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -22,4 +19,7 @@ public class Fees extends BaseEntity{
     Float amount;
 
     Date datePaid;
+    @OneToOne
+    @JoinColumn(name = "fees_id", referencedColumnName = "id")// defining the foreign key which is ID
+    Student student;
 }
