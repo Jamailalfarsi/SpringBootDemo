2adb7919c3e1449dd07aaf2ba46863979bb21df8
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-04-17 11:48:33 +0400

    Added  @Autowired into FeesController

--- demo/src/main/java/com/example/demo/controllers/FeesController.java
+++ demo/src/main/java/com/example/demo/controllers/FeesController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controllers;
 
+import com.example.demo.Repositories.FeesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FeesController {
+    @Autowired
+    FeesRepository feesRepository;
 }
