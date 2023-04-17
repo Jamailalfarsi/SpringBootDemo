1b4143dd8095046bd5b78180487a27feb4e6eb1f
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-04-17 11:51:21 +0400

    Added  @Autowired into FeesController

--- demo/src/main/java/com/example/demo/controllers/FeesController.java
+++ demo/src/main/java/com/example/demo/controllers/FeesController.java
@@ -1,11 +1,20 @@
 package com.example.demo.controllers;
 
+import com.example.demo.Models.Fees;
+import com.example.demo.Models.School;
 import com.example.demo.Repositories.FeesRepository;
+import com.example.demo.Services.FeesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class FeesController {
     @Autowired
-    FeesRepository feesRepository;
+    FeesService feesService;
+
+   
 }
