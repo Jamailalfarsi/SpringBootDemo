6cf511f0dbf10b27b99f4187d979ea550f3d3cd4
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-04-18 09:49:34 +0400

    Added @Getter, @Setter,@Data in CourseAverageMarkDTO

--- demo/src/main/java/com/example/demo/DTO/CourseAverageMarkDTO.java
+++ demo/src/main/java/com/example/demo/DTO/CourseAverageMarkDTO.java
@@ -1,5 +1,15 @@
 package com.example.demo.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+
 public class CourseAverageMarkDTO {
     String courseName;
     Integer obtainedMarks;
